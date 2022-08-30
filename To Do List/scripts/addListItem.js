90caf20e37828bf80b0d798c6b4c7292c2406598
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-08-30 18:27:57 +0430

    basic

---
+++ To Do List/scripts/addListItem.js
@@ -0,0 +1,55 @@
+import attachEventListeners from "./attachEventListeners.js";
+import { images } from "./variables.js";
+
+const addListItem = (todoText) => {
+  const list = document.querySelector(".list");
+
+  const itemDiv = document.createElement("div");
+  itemDiv.classList.add("item");
+
+  const checkItemDiv = document.createElement("div");
+  checkItemDiv.classList.add("check-item-box");
+  itemDiv.appendChild(checkItemDiv);
+
+  const checkItemImgBtn = document.createElement("img");
+  checkItemImgBtn.classList.add("check-item-img-btn");
+  checkItemImgBtn.src = images.uncheckedImgPath;
+  checkItemImgBtn.dataset.checked = false;
+  checkItemDiv.appendChild(checkItemImgBtn);
+
+  const listItemText = document.createElement("div");
+  listItemText.classList.add("list-item-text");
+  listItemText.innerText = todoText;
+  itemDiv.appendChild(listItemText);
+
+  const deleteItemDiv = document.createElement("div");
+  deleteItemDiv.classList.add("delete-item-box");
+  itemDiv.appendChild(deleteItemDiv);
+
+  const deleteItemImgBtn = document.createElement("img");
+  deleteItemImgBtn.classList.add("delete-item-img-btn");
+  deleteItemImgBtn.src = images.deleteImgPath;
+  deleteItemDiv.appendChild(deleteItemImgBtn);
+
+  list.appendChild(itemDiv);
+
+  attachEventListeners(checkItemImgBtn, deleteItemImgBtn);
+};
+
+export default addListItem;
+
+/*
+ <div class="item">
+    <div class="check-item-box">
+        <img
+        src="./images/unchecked.png"
+        class="check-item-img-btn"
+        data-checked="false"
+        />
+    </div>
+    <div class="list-item-text">Do Homework</div>
+    <div class="delete-item-box">
+        <img src="./images/delete.png" class="delete-item-img-btn" />
+    </div>
+</div>
+*/
