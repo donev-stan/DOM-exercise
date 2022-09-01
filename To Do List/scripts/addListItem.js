71b0bf54d5536db50ec6bef602075f896e353116
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-01 21:08:51 +0300

    unselectable class and selection color change

--- To Do List/scripts/addListItem.js
+++ To Do List/scripts/addListItem.js
@@ -2,58 +2,53 @@ import attachEventListeners from "./attachEventListeners.js";
 import elements from "./domElements.js";
 import generateTaskId from "./generateTaskId.js";
 import { pushTaskToLocalStorage } from "./localStorage.js";
-import {
-  images,
-  incrementDoneTasks,
-  incrementOngoingTasks,
-} from "./variables.js";
+import { images, incrementDoneTasks, incrementOngoingTasks } from "./variables.js";
 
 const addListItem = (todoText, idFromLocal, checkedFromLocal) => {
-  const list = document.querySelector(".list");
+	const list = document.querySelector(".list");
 
-  const itemDiv = document.createElement("div");
-  itemDiv.classList.add("item");
+	const itemDiv = document.createElement("div");
+	itemDiv.classList.add("item");
 
-  const itemIdDiv = document.createElement("div");
-  itemIdDiv.classList.add("task-id");
-  const taskId = generateTaskId();
-  itemIdDiv.dataset.id = idFromLocal ? idFromLocal : taskId;
-  itemDiv.appendChild(itemIdDiv);
+	const itemIdDiv = document.createElement("div");
+	itemIdDiv.classList.add("task-id");
+	const taskId = generateTaskId();
+	itemIdDiv.dataset.id = idFromLocal ? idFromLocal : taskId;
+	itemDiv.appendChild(itemIdDiv);
 
-  const deleteItemDiv = document.createElement("div");
-  deleteItemDiv.classList.add("delete-item-box");
-  itemDiv.appendChild(deleteItemDiv);
+	const deleteItemDiv = document.createElement("div");
+	deleteItemDiv.classList.add("delete-item-box");
+	deleteItemDiv.classList.add("unselectable");
+	itemDiv.appendChild(deleteItemDiv);
 
-  const deleteItemImgBtn = document.createElement("img");
-  deleteItemImgBtn.classList.add("delete-item-img-btn");
-  deleteItemImgBtn.src = images.deleteImgPath;
-  deleteItemDiv.appendChild(deleteItemImgBtn);
+	const deleteItemImgBtn = document.createElement("img");
+	deleteItemImgBtn.classList.add("delete-item-img-btn");
+	deleteItemImgBtn.src = images.deleteImgPath;
+	deleteItemDiv.appendChild(deleteItemImgBtn);
 
-  const checkItemDiv = document.createElement("div");
-  checkItemDiv.classList.add("check-item-box");
-  itemDiv.appendChild(checkItemDiv);
+	const checkItemDiv = document.createElement("div");
+	checkItemDiv.classList.add("check-item-box");
+	checkItemDiv.classList.add("unselectable");
+	itemDiv.appendChild(checkItemDiv);
 
-  const checkItemImgBtn = document.createElement("img");
-  checkItemImgBtn.classList.add("check-item-img-btn");
-  checkItemImgBtn.dataset.checked = checkedFromLocal ? true : false;
-  checkItemImgBtn.src = checkedFromLocal
-    ? images.checkedImgPath
-    : images.uncheckedImgPath;
-  checkItemDiv.appendChild(checkItemImgBtn);
+	const checkItemImgBtn = document.createElement("img");
+	checkItemImgBtn.classList.add("check-item-img-btn");
+	checkItemImgBtn.dataset.checked = checkedFromLocal ? true : false;
+	checkItemImgBtn.src = checkedFromLocal ? images.checkedImgPath : images.uncheckedImgPath;
+	checkItemDiv.appendChild(checkItemImgBtn);
 
-  const listItemText = document.createElement("div");
-  listItemText.classList.add("list-item-text");
-  listItemText.innerText = todoText;
-  itemDiv.appendChild(listItemText);
+	const listItemText = document.createElement("div");
+	listItemText.classList.add("list-item-text");
+	listItemText.innerText = todoText;
+	itemDiv.appendChild(listItemText);
 
-  list.appendChild(itemDiv);
+	list.appendChild(itemDiv);
 
-  checkedFromLocal ? incrementDoneTasks() : incrementOngoingTasks();
+	checkedFromLocal ? incrementDoneTasks() : incrementOngoingTasks();
 
-  attachEventListeners(checkItemImgBtn, deleteItemImgBtn);
+	attachEventListeners(checkItemImgBtn, deleteItemImgBtn);
 
-  if (!idFromLocal)
-    pushTaskToLocalStorage({ id: taskId, checked: false, todoText });
+	if (!idFromLocal) pushTaskToLocalStorage({ id: taskId, checked: false, todoText });
 };
 
 export default addListItem;
