3f2e5b7afa263cd364cdbfd470d97967a2462b3f
Stanislav Donev <77002111+donev-stan@users.noreply.github.com>
2022-09-03 13:06:31 +0300

    dom elements ref

--- Register Form/scripts/domElements.js
+++ Register Form/scripts/domElements.js
@@ -1,3 +1,19 @@
-const element = {};
+const elements = {
+	registerBtn: () => document.getElementById("register-btn"),
 
-export default element;
+	countryInput: () => document.getElementById("country"),
+	zipInput: () => document.getElementById("zip"),
+	telInput: () => document.getElementById("tel"),
+	lastNameInput: () => document.getElementById("last-name"),
+	firstNameInput: () => document.getElementById("first-name"),
+};
+
+const errorElements = {
+	firstNameError: () => document.querySelector(".first-name-error-msg"),
+	lastNameError: () => document.querySelector(".last-name-error-msg"),
+	telError: () => document.querySelector(".tel-error-msg"),
+	zipError: () => document.querySelector(".zip-error-msg"),
+	countryError: () => document.querySelector(".country-error-msg"),
+};
+
+export { elements, errorElements };
